b3fa12b26328c2f830fbb9a23ff48780f9530143
Pedro Martin <pataruco@gmail.com>
2015-07-10 21:16:17 +0100

    menu event listerners done

--- app/assets/javascripts/peace_menus.js
+++ app/assets/javascripts/peace_menus.js
@@ -1,4 +1,13 @@
 $(document).ready(function(){
 	$('#hdi').on('click', function(){console.log('hdi clicked')});
-	$('#edu_index').on('click', function(){console.log('inside edu index')})
-})
+	$('#edu_index').on('click', function(){console.log('inside edu index')});
+	$('#heal_index').on('click', function(){console.log('inside heal index')});
+	$('#heal_exp').on('click', function(){console.log('inside heal exp')});
+	$('#life').on('click', function(){console.log('inside life')});
+	$('#gdp').on('click', function(){console.log('inside gdp')})
+});
+
+
+
+
+	
