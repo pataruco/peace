edb87477531442119d73afe37038699e012d5738
Pedro Martin <pataruco@gmail.com>
2015-07-10 13:58:29 +0100

    able to fetch data from the databse, moving to style

--- app/assets/javascripts/peace_view.js
+++ app/assets/javascripts/peace_view.js
@@ -1,7 +1,6 @@
 // Variables
 country_data = [];
 
-
 $(document).ready(function() {
 
 	//Rendering the map
@@ -15,41 +14,27 @@ $(document).ready(function() {
             });
         },
 
-        data: {country_data}, 
-
-        geographyConfig: {
-        	popupTemplate: function(geo, data) {
-        		if (data !== null) {
-        			return ['<div class="hoverinfo"><strong>',
-        			'Number of things in ' + geo.properties.name,
-        			': ' + data.numberOfThings,
-        			'</strong></div>'].join('');
-        			console.log(data);
-        		}
-        	},
-            highlightFillColor: 'grey',
-            borderColor: 'white',
-            borderWidth: 0
-        },
 	}); // end of map
 
 
 	// fetch the data from database on click on map and storing in a global variables
 	function dataOnInfoWindow (country_code){
-	$.ajax({
-		type: "GET",
-		contentType: "application/json; charset=utf-8",
-		url: 'peaces/show/' + country_code,
-		dataType: 'json',
-		success: function (data) {
-			country_data = data;
-		},
-		error: function (result) {
-			console.log('error');
-			console.log(result);
-		}
-    });
-}
+		$.ajax({
+			type: "GET",
+			contentType: "application/json; charset=utf-8",
+			url: 'peaces/show/' + country_code,
+			dataType: 'json',
+			success: function (data) {
+				country_data = data;
+				console.log(country_data[0]);
+			},
+			error: function (result) {
+				console.log('error');
+				console.log(result);
+			}
+	    });
+
+	}
 
 	//Make map responsive
 	$(window).on('resize', function() {
