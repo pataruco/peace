da860e572e88fbad81a5c2bfb8f6fc12d2755c56
Pedro Martin <pataruco@gmail.com>
2015-07-10 11:00:47 +0100

    On a click on a map is fetching the country data from database

--- app/assets/javascripts/peace_view.js
+++ app/assets/javascripts/peace_view.js
@@ -1,18 +1,8 @@
-$.ajax({
-            type: "GET",
-            contentType: "application/json; charset=utf-8",
-            url: 'peaces/show/VEN',
-            dataType: 'json',
-            success: function (data) {
-            	console.log('success');
-            	console.log(data);
-            	
-            },
-            error: function (result) {
-            	console.log('error');
-            	console.log(result);
-            }
-    });
+
+
+
+
+
 
 
 
@@ -27,12 +17,30 @@ $(document).ready(function() {
 			datamap.svg.selectAll('.datamaps-subunit').on('click', function(geography) {
 				console.log(geography.properties.name);
                 country_code = (geography.id);
-                console.log(country_code)
-                
+                console.log(country_code);
+                dataOnInfoWindow(country_code);
+
 
             });
         }
 	});
+
+	function dataOnInfoWindow (country_code){
+	$.ajax({
+		type: "GET",
+		contentType: "application/json; charset=utf-8",
+		url: 'peaces/show/' + country_code,
+		dataType: 'json',
+		success: function (data) {
+			console.log('success');
+			console.log(data);
+		},
+		error: function (result) {
+			console.log('error');
+			console.log(result);
+		}
+    });
+}
 
 	//Make map responsive
 	$(window).on('resize', function() {
