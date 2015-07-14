6e13afd116776efae924b7ca3df77e6209f6fea7
Pedro Martin <pataruco@gmail.com>
2015-07-14 22:44:33 +0100

    legend refactored

--- app/assets/javascripts/peace_render_map.js
+++ app/assets/javascripts/peace_render_map.js
@@ -1,13 +1,13 @@
 $(document).ready(function() {
 	// Variables  //
 	var legend = {
-		hdi_index: {title: 'Human Development Index', colors: ['#fff7ec', '#fee8c8', '#fdd49e', '#fdbb84', '#fc8d59', '#ef6548', '#d7301f', '#b30000', '#7f0000']},
-		edu_index: {title: 'Education Index', colors: ['#f7fcfd', '#e0ecf4', '#bfd3e6', '#9ebcda', '#8c96c6', '#8c6bb1', '#88419d', '#810f7c', '#4d004b']},
-		heal_index: {title: 'Health Index', colors: ['#f7fcf0', '#e0f3db', '#ccebc5', '#a8ddb5', '#7bccc4', '#4eb3d3', '#2b8cbe', '#0868ac', '#084081']},
-		edu_exp: {title: 'Education Expenditure', colors: ['#f7fcfd', '#e5f5f9', '#ccece6', '#99d8c9', '#66c2a4', '#41ae76', '#238b45', '#006d2c', '#00441b']},
-		heal_exp: {title: 'Health Expenditure', colors: ['#fff7fb', '#ece7f2', '#d0d1e6', '#a6bddb', '#74a9cf', '#3690c0', '#0570b0', '#045a8d', '#023858']},
-		life: {title: 'Life Expectancy', colors: ['#ffffcc', '#ffeda0', '#fed976', '#feb24c', '#fd8d3c', '#fc4e2a', '#e31a1c', '#bd0026', '#800026']},
-		gdp: {title: 'GDP', colors: ['#fff7f3', '#fde0dd', '#fcc5c0', '#fa9fb5', '#f768a1', '#dd3497', '#ae017e', '#7a0177', '#49006a']}
+		hdi_index: {title: 'Human Development Index', colors: ['#fff7ec', '#fc8d59', '#7f0000']},
+		edu_index: {title: 'Education Index', colors: ['#f7fcfd', '#8c6bb1', '#4d004b']},
+		heal_index: {title: 'Health Index', colors: ['#f7fcf0', '#7bccc4', '#084081']},
+		edu_exp: {title: 'Education Expenditure', colors: ['#f7fcfd', '#66c2a4', '#00441b']},
+		heal_exp: {title: 'Health Expenditure', colors: ['#fff7fb', '#74a9cf', '#023858']},
+		life: {title: 'Life Expectancy', colors: ['#ffffcc', '#fd8d3c', '#800026']},
+		gdp: {title: 'GDP', colors: ['#fff7f3', '#f768a1', '#49006a']}
 	}
 
 	//event listeners //
@@ -111,8 +111,8 @@ $(document).ready(function() {
 			var max = d3.max(indicatorsArray);
 
 			var colorMin = legend[indicator].colors[0]; 
-			var colorMean = legend[indicator].colors[4]; 
-			var colorMax = legend[indicator].colors[8];
+			var colorMean = legend[indicator].colors[1]; 
+			var colorMax = legend[indicator].colors[2];
 
 			var scale = d3.scale.linear()
     			.domain([min, mean, max])
