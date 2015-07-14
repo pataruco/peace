dc6a8d34d953d929721b58e1535954ccd3bd86f1
Pedro Martin <pataruco@gmail.com>
2015-07-14 10:55:54 +0100

    Add black to countries data witth nil as colout value

--- app/assets/javascripts/peace_render_map.js
+++ app/assets/javascripts/peace_render_map.js
@@ -83,61 +83,10 @@ $(document).ready(function() {
 				console.log(data);
 			}
 		}).done(function(data){
-			renderNewMap(data, indicator);
+			renderNewChoroplethMap(data, indicator);
 		});// end of done
 
 
-		function renderNewMap(data, indicator) {
-			$('.datamaps').hide();
-
-			var map = new Datamap({
-
-				element: document.getElementById('map-container'),
-
-				projection: 'mercator',
-
-				fills: {defaultFill: 'F4FDFF'},
-
-				geographyConfig: {
-					borderColor: 'white',
-		            borderWidth: 0,
-			        highlightFillColor: 'white',
-			        highlightBorderColor: 'black',
-			        highlightBorderWidth: 1,
-			        popupOnHover: false,
-		        },
-
-				done: function(datamap) {
-					datamap.svg.selectAll('.datamaps-subunit').on('click', function(geography) {
-						var xCoord = d3.event.pageX;
-						var yCoord = d3.event.pageY;
-						$.ajax({
-							type: "GET",
-							contentType: "application/json; charset=utf-8",
-							url: 'peaces/show/' + geography.id,
-							dataType: 'json',
-							success: function (data) {
-								country_data = data;
-								console.log(country_data[0]);
-							},
-							error: function (result) {
-								console.log('error');
-								console.log(result);
-							}
-						}).done(function(data){
-							$("#info-window").text(country_data[0].country_name);
-		        			var infoWindow = $("#info-window");
-		        			infoWindow.css({
-		        				position:"absolute", 
-		        				top: yCoord, // need x and y coordinates from click
-		        				left: xCoord
-        					});
-        					renderNewChoroplethMap(data, indicator);
-	    				});		
-        			});
-				},
-			});
-		}// end of renderNewMaP
 
 		function renderNewChoroplethMap(data, indicator) {
 			var colors = {};
