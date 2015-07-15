ca6a5f03b9911e1a2d5975363f443d1e09c5fe69
Pedro Martin <pataruco@gmail.com>
2015-07-15 22:13:09 +0100

    education index it's working

--- app/assets/javascripts/peace_render_map.js
+++ app/assets/javascripts/peace_render_map.js
@@ -184,7 +184,67 @@ $(document).ready(function() {
 							  attr("transform", "translate(0, 18)").
 							  attr("class", "yAxis");
 //*****************************************************************************************************************************
-					
+//************************** render EDUCATION INDEX D3 Visualization inside a div**************************************************
+						    var barWidth = 60;
+							var width = (barWidth + 10) * edu_index_data.length;
+							var height = 100;
+
+							var x = d3.scale.linear().domain([0, edu_index_data.length]).range([0, width]);
+							var y = d3.scale.linear().domain([0, d3.max(edu_index_data, function(datum) { return datum.edu_index})]).
+								rangeRound([0, height]);
+
+							// add the canvas to the DOM
+							var eduIndexBar = d3.select("#edu_index_data").
+							append("svg:svg").
+							attr("width", width).
+							attr("height", 230);
+
+							eduIndexBar.selectAll("rect").
+							data(edu_index_data).
+							enter().
+							append("svg:rect").
+							attr("x", function(datum, index) { return x(index); }).
+							attr("y", function(datum) { return height - y(datum.edu_index); }).
+							attr("height", function(datum) { return y(datum.edu_index); }).
+							attr("width", barWidth).
+							attr("fill", "#2d578b");
+
+							// numbers on bars
+
+							eduIndexBar.selectAll("text").
+							data(edu_index_data).
+							enter().
+							append("svg:text").
+							attr("x", function(datum, index) { return x(index) + barWidth; }).
+							attr("y", function(datum) { return height - y(datum.edu_index); }).
+							attr("dx", -barWidth/2).
+							attr("dy", "1.2em").
+							attr("text-anchor", "middle").
+							text(function(datum) { return datum.edu_index;}).
+							attr("fill", "white");
+
+							// scale
+
+							eduIndexBar.selectAll("text.yAxis").
+							data(edu_index_data).
+							enter().append("svg:text").
+							attr("x", function(datum, index) { return x(index) + barWidth; }).
+							attr("y", height).
+							attr("dx", -barWidth/2).
+							attr("text-anchor", "middle").
+							attr("style", "font-size: 12; font-family: Helvetica, sans-serif").
+							text(function(datum) { return datum.scope;}).
+							attr("transform", "translate(0, 18)").
+							attr("class", "yAxis");	
+//*****************************************************************************************************************************				
+
+
+
+
+
+
+
+
 						}// RenderInfoCountryWindow
 
 	    			}// end infoCountryWindow	
--- rendering_D3.js
+++ rendering_D3.js
@@ -4,48 +4,48 @@ var height = 100;
 
 var x = d3.scale.linear().domain([0, hdi_index_data.length]).range([0, width]);
 var y = d3.scale.linear().domain([0, d3.max(hdi_index_data, function(datum) { return datum.hdi_index})]).
-  rangeRound([0, height]);
+rangeRound([0, height]);
 
+// add the canvas to the DOM
+var hdiBar = d3.select("#hdi_index_data").
+append("svg:svg").
+attr("width", width).
+attr("height", 230);
 
-var barDemo = d3.select("#hdi_index_data").
-  append("svg:svg").
-  attr("width", width).
-  attr("height", 230);
+hdiBar.selectAll("rect").
+data(hdi_index_data).
+enter().
+append("svg:rect").
+attr("x", function(datum, index) { return x(index); }).
+attr("y", function(datum) { return height - y(datum.hdi_index); }).
+attr("height", function(datum) { return y(datum.hdi_index); }).
+attr("width", barWidth).
+attr("fill", "#2d578b");
 
-barDemo.selectAll("rect").
-  data(hdi_index_data).
-  enter().
-  append("svg:rect").
-  attr("x", function(datum, index) { return x(index); }).
-  attr("y", function(datum) { return height - y(datum.hdi_index); }).
-  attr("height", function(datum) { return y(datum.hdi_index); }).
-  attr("width", barWidth).
-  attr("fill", "#2d578b");
+// numbers on bars
 
-  // numbers on bars
-
-  barDemo.selectAll("text").
-  data(hdi_index_data).
-  enter().
-  append("svg:text").
-  attr("x", function(datum, index) { return x(index) + barWidth; }).
-  attr("y", function(datum) { return height - y(datum.hdi_index); }).
-  attr("dx", -barWidth/2).
-  attr("dy", "1.2em").
-  attr("text-anchor", "middle").
-  text(function(datum) { return datum.hdi_index;}).
-  attr("fill", "white");
+hdiBar.selectAll("text").
+data(hdi_index_data).
+enter().
+append("svg:text").
+attr("x", function(datum, index) { return x(index) + barWidth; }).
+attr("y", function(datum) { return height - y(datum.hdi_index); }).
+attr("dx", -barWidth/2).
+attr("dy", "1.2em").
+attr("text-anchor", "middle").
+text(function(datum) { return datum.hdi_index;}).
+attr("fill", "white");
 
 // scale
 
-barDemo.selectAll("text.yAxis").
-  data(hdi_index_data).
-  enter().append("svg:text").
-  attr("x", function(datum, index) { return x(index) + barWidth; }).
-  attr("y", height).
-  attr("dx", -barWidth/2).
-  attr("text-anchor", "middle").
-  attr("style", "font-size: 12; font-family: Helvetica, sans-serif").
-  text(function(datum) { return datum.scope;}).
-  attr("transform", "translate(0, 18)").
-  attr("class", "yAxis");
+hdiBar.selectAll("text.yAxis").
+data(hdi_index_data).
+enter().append("svg:text").
+attr("x", function(datum, index) { return x(index) + barWidth; }).
+attr("y", height).
+attr("dx", -barWidth/2).
+attr("text-anchor", "middle").
+attr("style", "font-size: 12; font-family: Helvetica, sans-serif").
+text(function(datum) { return datum.scope;}).
+attr("transform", "translate(0, 18)").
+attr("class", "yAxis");
