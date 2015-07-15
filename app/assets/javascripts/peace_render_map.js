1a91f24418463c7610bb1243dfa34fda2c755d8c
Pedro Martin <pataruco@gmail.com>
2015-07-15 22:22:50 +0100

    education exp working

--- app/assets/javascripts/peace_render_map.js
+++ app/assets/javascripts/peace_render_map.js
@@ -290,6 +290,58 @@ $(document).ready(function() {
 							attr("transform", "translate(0, 18)").
 							attr("class", "yAxis");
 
+//************************** render edu_exp D3 Visualization inside a div**************************************************
+						    var barWidth = 60;
+							var width = (barWidth + 10) * edu_exp_data.length;
+							var height = 100;
+
+							var x = d3.scale.linear().domain([0, edu_exp_data.length]).range([0, width]);
+							var y = d3.scale.linear().domain([0, d3.max(edu_exp_data, function(datum) { return datum.edu_exp})]).
+								rangeRound([0, height]);
+
+							// add the canvas to the DOM
+							var eduExpBar = d3.select("#edu_exp_data").
+							append("svg:svg").
+							attr("width", width).
+							attr("height", 230);
+
+							eduExpBar.selectAll("rect").
+							data(edu_exp_data).
+							enter().
+							append("svg:rect").
+							attr("x", function(datum, index) { return x(index); }).
+							attr("y", function(datum) { return height - y(datum.edu_exp); }).
+							attr("height", function(datum) { return y(datum.edu_exp); }).
+							attr("width", barWidth).
+							attr("fill", "#2d578b");
+
+							// numbers on bars
+
+							eduExpBar.selectAll("text").
+							data(edu_exp_data).
+							enter().
+							append("svg:text").
+							attr("x", function(datum, index) { return x(index) + barWidth; }).
+							attr("y", function(datum) { return height - y(datum.edu_exp); }).
+							attr("dx", -barWidth/2).
+							attr("dy", "1.2em").
+							attr("text-anchor", "middle").
+							text(function(datum) { return datum.edu_exp;}).
+							attr("fill", "white");
+
+							// scale
+
+							eduExpBar.selectAll("text.yAxis").
+							data(edu_exp_data).
+							enter().append("svg:text").
+							attr("x", function(datum, index) { return x(index) + barWidth; }).
+							attr("y", height).
+							attr("dx", -barWidth/2).
+							attr("text-anchor", "middle").
+							attr("style", "font-size: 12; font-family: Helvetica, sans-serif").
+							text(function(datum) { return datum.scope;}).
+							attr("transform", "translate(0, 18)").
+							attr("class", "yAxis");
 
 
 
